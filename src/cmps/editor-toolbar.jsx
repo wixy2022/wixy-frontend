cbeed69615d105c4044e3f3c207d6575b1ab95f2
AlexYa <1yakovlevalex@gmail.com>
2022-05-26 19:29:16 +0300

    fixed bug at template-tool-bar

--- src/cmps/editor-toolbar.jsx
+++ src/cmps/editor-toolbar.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
 import { Draggable } from "react-beautiful-dnd"
 import { DraggableTemplate } from './draggable-template.jsx'
+import { utilService } from "../services/util.service.js"
 
-
-export const TemplateToolBar = ({ setToolBarMode, templates,setTemplateKey }) => {
+export const TemplateToolBar = ({ setToolBarMode, templates, setTemplateKey }) => {
     const [selectedTemplates, setSelectedTemplates] = useState(null)
     const [currTopic, setCurrTopic] = useState(null)
 
@@ -11,38 +11,36 @@ export const TemplateToolBar = ({ setToolBarMode, templates,setTemplateKey }) =>
 
     const onHandleTemplates = async (ev) => {
         ev.stopPropagation()
-        const name = ev.target.name
+        const { name } = ev.target
         if (currTopic === name) {
+            setCurrTopic(null)
             setTemplateKey('')
             setToolBarMode('')
-            setCurrTopic(null)
             setSelectedTemplates(null)
         } else {
+            setCurrTopic(`${name}`)
             setTemplateKey(`${name}`)
-            await setCurrTopic(name)
-            await  setSelectedTemplates(templates[name])
+            setSelectedTemplates(templates[name])
             setToolBarMode('tool-bar-open')
         }
     }
     const getToolBarButtons = () => {
         const names = ['header', 'section', 'text', 'cards', 'gallery', 'form', 'map', 'chat', 'video', 'footer',]
-        return names.map((name, idx) => <div onClick={onHandleTemplates}
+        return names.map((name) => <div
             className='editor-icon-container'
             key={name} name={name} >
-            <a name={name} className={`editor-icon-img ${name}`} /></div>)
+            <a name={name} onClick={onHandleTemplates} className={`editor-icon-img ${name}`} /></div>)
     }
 
     console.log(selectedTemplates)
-
     return <section className="template-tool-bar" >
         <div className="template-bar-btns">
             {getToolBarButtons()}
         </div>
 
-        <div className="tool-bar-options">
-
+        <div className={`tool-bar-options`}>
             {selectedTemplates && selectedTemplates.map((template, idx) => {
-                return <Draggable style={{ width: '250px', height: '100px' }} key={idx + template.id + 'template'} draggableId={template.id + 'template'} index={idx + 100}>
+                return <Draggable style={{ width: '250px', height: '100px' }} key={utilService.createKey() + 'template'} draggableId={template.id + 'template'} index={idx + 100}>
                     {(provided) => {
                         return <DraggableTemplate
                             className="template-editor-display"
@@ -55,6 +53,20 @@ export const TemplateToolBar = ({ setToolBarMode, templates,setTemplateKey }) =>
                 </Draggable>
 
             })}
+            {/* {selectedTemplates && selectedTemplates.map((template, idx) => {
+                return <Draggable style={{ width: '250px', height: '100px' }} key={idx + template.id + 'template'} draggableId={template.id + 'template'} index={idx + 100}>
+                    {(provided) => {
+                        return <DraggableTemplate
+                            className="template-editor-display"
+                            props1={provided.draggableProps}
+                            template={template}
+                            props2={provided.dragHandleProps}
+                            forwardref={provided.innerRef}
+                        />
+                    }}
+                </Draggable>
+
+            })} */}
         </div>
     </section>
 
