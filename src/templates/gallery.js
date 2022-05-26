484fd460b8403d635e45f9e9d2eeb397e885a154
AlexYa <1yakovlevalex@gmail.com>
2022-05-26 19:51:47 +0300

    created demo data for presentation

--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -1,12 +1,12 @@
 import { utilService } from "../services/util.service"
-
-export const wapGallery01 = {
+// to change only for display at demo
+export const wapGallery0102 = {
     id: utilService.makeId(),
     type: 'container-draggable',
     category: 'wap-gallery',
     theme: 'dramatic',
     className: 'wap-gallery-01',
-    imgUrl: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580956/templates/gallery-t1_gbrpql.png',
+    imgUrl: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653583654/templates/gallery-t1_sqxhz2.jpg ',
     cmps: [
         {
             id: utilService.makeId(),
@@ -33,4 +33,111 @@ export const wapGallery01 = {
             className: 'dramatic-gallery-img',
         }
     ]
+
 }
+
+
+// TEMPORARY UNTIL WE GET OUR DATA BASE AND CREATE SITE ///////
+
+export const wapGallery01 = [{
+    id: utilService.makeId(),
+    type: 'container-draggable',
+    category: 'wap-gallery',
+    theme: 'dramatic',
+    className: 'wap-gallery-01',
+    imgUrl: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653583654/templates/gallery-t1_sqxhz2.jpg ',
+    cmps: [
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery5_jjaep9.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580995/templates/gallery2_ncaykr.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery3_my49er.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery4_vbz7li.jpg',
+            className: 'dramatic-gallery-img',
+        }
+    ]
+
+},
+{
+    id: utilService.makeId(),
+    type: 'container-draggable',
+    category: 'wap-gallery',
+    theme: 'dramatic',
+    className: 'wap-gallery-01',
+    imgUrl: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653583654/templates/gallery-t1_sqxhz2.jpg ',
+    cmps: [
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery5_jjaep9.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580995/templates/gallery2_ncaykr.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery3_my49er.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery4_vbz7li.jpg',
+            className: 'dramatic-gallery-img',
+        }
+    ]
+},{
+    id: utilService.makeId(),
+    type: 'container-draggable',
+    category: 'wap-gallery',
+    theme: 'dramatic',
+    className: 'wap-gallery-01',
+    imgUrl: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653583654/templates/gallery-t1_sqxhz2.jpg ',
+    cmps: [
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery5_jjaep9.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580995/templates/gallery2_ncaykr.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery3_my49er.jpg',
+            className: 'dramatic-gallery-img',
+        },
+        {
+            id: utilService.makeId(),
+            type: 'img',
+            url: 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653580996/templates/gallery4_vbz7li.jpg',
+            className: 'dramatic-gallery-img',
+        }
+    ]
+}]
