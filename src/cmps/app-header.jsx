862db23b78521eaba68708898df4e7dbf20bae5a
AlexYa <1yakovlevalex@gmail.com>
2022-05-29 20:28:21 +0300

    functional and responsive header

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -2,17 +2,29 @@ import { NavLink, Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState } from "react"
 import { setScreen } from "../store/actions/screen.action"
+import {logout} from '../store/actions/user.action'
+import { useHistory } from "react-router-dom"
 // import { useState } from "react"
 // import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 // import logoImg from '../assets/img/logo.png'
 
 export const AppHeader = () => {
     const { user } = useSelector(storeState => storeState.userModule)
+    const dispatch = useDispatch()
+    const history = useHistory()
     const defaultUrl = 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653772907/icons/login-image_nnfkgq.png'
-    console.log(user)
-    const [headerClass,setHeaderClass] = useState('')
-    const isOpen = headerClass? '':'open'
+    const [headerClass, setHeaderClass] = useState('')
+    const isOpen = headerClass ? '' : 'open'
+    const isLogin = user? 'Logout':'Login'
 
+    const onSetLoginLogout =()=>{
+        if(isLogin==='login'){
+            dispatch(logout)
+        }else{
+            console.log(history)
+            history.push('/login')
+        }
+    }
     return <section className={`app-header ${headerClass}`}>
         <main className="flex align-center">
             {/* <Link to="/"><img src={logoImg} alt="Wixy" className="logo" /></Link> */}
@@ -21,19 +33,29 @@ export const AppHeader = () => {
             </div></Link>
             <nav className="header-nav">
                 <NavLink to='/' exact>Home</NavLink>
-                {/* <NavLink to='/about'>About</NavLink> */}
                 <NavLink to='/templates'>Templates</NavLink>
                 <NavLink to='/editor'>Editor</NavLink>
-                {user&&<>
-                <NavLink to='/my-sites'>My Sites</NavLink>
-                {/* <button>Logout</button> */}
-                </>
-                }
+               
             </nav>
-            <div  className="user-area">
-            <div onClick={()=>setHeaderClass(isOpen)} className="hambrger">☰</div>
+            <div onClick={() => setHeaderClass(isOpen)} className="user-area">
                 {!user && <Link className="login" to='/login'>Login</Link>}
-                {user && <img src={user?.imgUrl || defaultUrl}></img>}
+                {user && <img className="user-img-header" src={user?.imgUrl || defaultUrl}></img>}
+                <div className="hambrger">☰</div>
+                {isOpen&&<div className="user-modal-header">
+                    {user&&<div className="user-data">
+                    <img className="user-img-header-inside" src={user?.imgUrl || defaultUrl}></img>
+                    <div className="username">{`${user.lastName} ${user.firstName}`}</div>
+                    </div>
+                    }
+                    <div className="nav-links-user-modal">
+                    <NavLink className={'modal-links'} to='/' exact>Home</NavLink>
+                    <NavLink className={'modal-links'} to='/templates'>Templates</NavLink>
+                    <NavLink className={'modal-links'} to='/editor'>Editor</NavLink>
+                    {user &&  <NavLink to='/my-sites'>My Sites</NavLink>}
+                    </div>
+                    { <button onClick={onSetLoginLogout} className="logout-btn">{isLogin}</button>}
+                    {/* NOTE 2 rows above must stay apart (logout-btn and My-sites) */}
+                </div>}
             </div>
             {/* <div className="flex"> */}
             {/* <div className="profile-button center-text" onClick={onProfileButton}>
