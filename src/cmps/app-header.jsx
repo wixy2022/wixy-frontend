cb509f30299e230288272889a3834af92a410984
AlexYa <1yakovlevalex@gmail.com>
2022-05-29 14:59:00 +0300

    hamburger-created-in650px

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,6 +1,6 @@
 import { NavLink, Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { setScreen } from "../store/actions/screen.action"
 // import { useState } from "react"
 // import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
@@ -10,26 +10,32 @@ export const AppHeader = () => {
     const { user } = useSelector(storeState => storeState.userModule)
     const defaultUrl = 'https://res.cloudinary.com/drpqhjyvk/image/upload/v1653772907/icons/login-image_nnfkgq.png'
     console.log(user)
+    const [headerClass,setHeaderClass] = useState('')
+    const isOpen = headerClass? '':'open'
 
-
-    return <section className="app-header ">
+    return <section className={`app-header ${headerClass}`}>
         <main className="flex align-center">
             {/* <Link to="/"><img src={logoImg} alt="Wixy" className="logo" /></Link> */}
             <Link className="logo-link" to="/"><div className="logo">
                 <img src="https://res.cloudinary.com/drpqhjyvk/image/upload/v1653740284/logo-wixy_fafsy0.png" alt="" />
             </div></Link>
-            {/* <div className="flex"> */}
             <nav className="header-nav">
                 <NavLink to='/' exact>Home</NavLink>
-                <NavLink to='/about'>About</NavLink>
+                {/* <NavLink to='/about'>About</NavLink> */}
                 <NavLink to='/templates'>Templates</NavLink>
                 <NavLink to='/editor'>Editor</NavLink>
+                {user&&<>
                 <NavLink to='/my-sites'>My Sites</NavLink>
+                {/* <button>Logout</button> */}
+                </>
+                }
             </nav>
-            <div className="user-area">
+            <div  className="user-area">
+            <div onClick={()=>setHeaderClass(isOpen)} className="hambrger">☰</div>
                 {!user && <Link className="login" to='/login'>Login</Link>}
                 {user && <img src={user?.imgUrl || defaultUrl}></img>}
             </div>
+            {/* <div className="flex"> */}
             {/* <div className="profile-button center-text" onClick={onProfileButton}>
                     {!user && <p></p>}
                     {user && <p>{getFirstLetterOfUser()}</p>}
