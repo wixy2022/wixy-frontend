7ea4febc67c5b021c953a801099b0366aaf27723
Ori Sason <orisason1@gmail.com>
2022-06-02 21:49:48 +0300

    added change text events to anchors

--- src/cmps/dynamic-cmps/anchor-cmp.jsx
+++ src/cmps/dynamic-cmps/anchor-cmp.jsx
@@ -1,10 +1,21 @@
 
-export const AnchorCmp = ({ cmp, onSelectActiveCmp, isPublish }) => {
+export const AnchorCmp = ({ cmp, onSelectActiveCmp, isPublish, onUpdateCmpTxt }) => {
+
+  const onFocusIn = (ev) => {
+    ev.stopPropagation()
+    onSelectActiveCmp(cmp, ev.target)
+}
+
+const onBlur = ({ target: { innerText } }) => {
+    onUpdateCmpTxt(innerText)
+}
+
   if (isPublish) return <a className={`anchor-cmp ${cmp.className}`} style={cmp.style}>{cmp.txt}</a>
 
   return <a className={`anchor-cmp ${cmp.className}`}
     target="_blank" style={cmp.style}
-    onClick={(ev) => onSelectActiveCmp(cmp, ev.target)}>
+    onFocus={onFocusIn} onBlur={onBlur}
+    contentEditable suppressContentEditableWarning={true}>
     {cmp.txt}
   </a>
 }
