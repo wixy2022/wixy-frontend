97f35ac25bd70dcdaf30233362c292fad7326d60
AlexYa <1yakovlevalex@gmail.com>
2022-06-07 01:17:09 +0300

    push-notifications-and-onclick-back-to-site

--- public/sw.js
+++ public/sw.js
@@ -6,10 +6,32 @@ self.addEventListener('push',(ev)=>{
     const data = ev.data?.json()
       console.log('Push recieved...')
       console.log(data.title,'data')
-
-      self.registration.showNotification('data.title', {
-        body : data.body,
-        icon: 'favicon.jpg',
-        onclick:console.log,
-      })
+  const options = {
+    body : data.body,
+    icon: 'favicon.jpg',
+    requireInteraction: true,
+    onclick:console.log,
+  }
+      self.registration.showNotification('data.title',options )
   })
+
+  self.addEventListener('notificationclick', function(event) {
+    let url = 'https://wixy-2022.herokuapp.com/#/';
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
+});
