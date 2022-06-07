781da608276f6cbe0e762e1590ce6ec070892fd9
Ori Sason <orisason1@gmail.com>
2022-06-07 11:06:01 +0300

    fixed homepage logo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,7 +12,7 @@ export function Home() {
         <section className="hero">
             <div className="get-started">
                 <div> Create a website with</div>
-                <div className="logo"><img src="https://res.cloudinary.com/drpqhjyvk/image/upload/v1653740284/logo-wixy_fafsy0.png" alt="" /></div>
+                <div className="logo"><img src="https://res.cloudinary.com/drpqhjyvk/image/upload/v1654588941/images/wixi-logo-transparent_mkd9if.png" alt="" /></div>
                 <Link to="/templates"><button className="get-started-btn">Get Started</button></Link>
             </div>
             <div className="hero-img"></div>
